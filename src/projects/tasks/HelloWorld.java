6db151d6e5b67ff758bea43288f29a21fdf81246
Галушка Игорь <galuropek@gmail.com>
2018-09-01 19:23:34 +0300

    Hello World!

---
+++ src/projects/tasks/HelloWorld.java
@@ -0,0 +1,10 @@
+package projects.tasks;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        JOptionPane.showMessageDialog(null, text);
+    }
+}
