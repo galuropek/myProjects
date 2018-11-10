bc9312f5061da09d55842977d41ed333f071fed1
Галушка Игорь <galuropek@gmail.com>
2018-11-10 13:04:16 +0300

    internationalization: add Messages class

---
+++ src/project/java/internationalization/Messages.java
@@ -0,0 +1,13 @@
+package project.java.internationalization;
+
+interface Messages {
+
+    String NAVBAR_HOME = "navbar.home";
+    String NAVBAR_RESET = "navbar.reset";
+    String NAVBAR_CARSLIST = "navbar.carsList";
+    String NAVBAR_NEWORDER = "navbar.newOrder";
+    String NAVBAR_ADDNEWCAR = "navbar.addNewCar";
+    String NAVBAR_LOGIN = "navbar.login";
+    String NAVBAR_REGISTRATION = "navbar.registration";
+    String NAVBAR_PROFILE = "navbar.profile";
+}
