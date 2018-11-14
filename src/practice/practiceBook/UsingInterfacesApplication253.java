048bc25c07c207fc052264747b33924e5cffece8
Галушка Игорь <galuropek@gmail.com>
2018-11-14 23:12:09 +0300

    practice tasks: implements interfaces

--- src/practice/practiceBook/UsingInterfacesApplication253.java
+++ src/practice/practiceBook/UsingInterfacesApplication253.java
@@ -10,5 +10,24 @@ interface Second {
     void hi();
 }
 
+class MyClass253 implements First, Second {
+
+    @Override
+    public void hello() {
+        System.out.println("First interface`s method");
+    }
+
+    @Override
+    public void hi() {
+        System.out.println("Second interface`s method");
+    }
+}
+
 public class UsingInterfacesApplication253 {
+    public static void main(String[] args) {
+
+        MyClass253 obj = new MyClass253();
+        obj.hello();
+        obj.hi();
+    }
 }
