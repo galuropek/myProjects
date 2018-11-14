3c99eb0fa7967a23c1fead03c315096809a7cc71
Галушка Игорь <galuropek@gmail.com>
2018-11-14 22:46:02 +0300

    practice tasks: interface

---
+++ src/practice/practiceBook/UsingInterfaceApplication.java
@@ -0,0 +1,11 @@
+package practice.practiceBook;
+
+interface MyInterface{
+
+    int NUMBER = 100;
+    int getNumber(double x);
+    char getSymbol(int n);
+}
+
+public class UsingInterfaceApplication {
+}
