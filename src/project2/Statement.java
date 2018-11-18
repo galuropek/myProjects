0157ae43a168cf1a37a8fb72ce3df6461346d9c5
Галушка Игорь <galuropek@gmail.com>
2018-11-18 14:41:10 +0300

    new project

--- src/project2/Statement.java
+++ src/project2/Statement.java
@@ -1,4 +1,19 @@
 package project2;
 
-public class Statement {
+public class Statement extends Message{
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public Statement(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void reaction() {
+        System.out.println("Это утверждение");
+    }
 }
