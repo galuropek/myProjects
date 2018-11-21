7c254c484eb18401e29b30f57156745b1eda39f4
Галушка Игорь <galuropek@gmail.com>
2018-11-21 17:19:34 +0300

    practice

--- src/practice/tasks/AvgLinkedList.java
+++ src/practice/tasks/AvgLinkedList.java
@@ -4,17 +4,26 @@ import java.util.LinkedList;
 
 public class AvgLinkedList {
 
-    static int getAverageLinkedList(LinkedList list) {
+    private static int getAverageLinkedList(LinkedList<Integer> list) {
 
         int mean = 0;
 
         int listSize = list.size();
 
         for (int i = 0; i < listSize; i++) {
-            if (i % 2 == 0) {
-                mean += 1;
+            if (i % 2 == 0 && i > 0) {
+                mean++;
             }
         }
-        return mean;
+        return list.get(mean);
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 11; i++) {
+            list.addLast((int) (Math.random() * 10 + 1));
+        }
+        System.out.println(list);
+        System.out.println(getAverageLinkedList(list));
     }
 }
