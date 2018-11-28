9ee65a84ffad8f935050b4d43e86af153306bae6
Галушка Игорь <galuropek@gmail.com>
2018-11-28 16:56:28 +0300

    practice OOP: Collector

---
+++ src/practice/gerbageRecycler/Collector.java
@@ -0,0 +1,18 @@
+package practice.gerbageRecycler;
+
+public class Collector {
+
+    void takeGarbage(Employee employee) {
+        if (employee instanceof Human) {
+            if (Util.testForGrbage()) {
+                System.out.println("All garbage is taken.");
+            } else {
+                System.out.println("This garbage is vary bad. It can`t be taken.");
+            }
+        } else if (employee instanceof Robot) {
+            System.out.println("All garbage is taken.");
+        } else if (employee instanceof Alien) {
+            System.out.println("This garbage is vary bad. It can`t be taken.");
+        }
+    }
+}
