0403da53e1500a9e22ad729c14590ae3bf8ffa2b
Галушка Игорь <galuropek@gmail.com>
2018-11-28 17:06:26 +0300

    practice OOP: Util

--- src/practice/gerbageRecycler/Util.java
+++ src/practice/gerbageRecycler/Util.java
@@ -4,9 +4,25 @@ public class Util {
 
     static boolean testForGrbage() {
         boolean result = false;
-        int random = (int) (Math.random());
+        int random = (int) (Math.random() * 2);
         if (random == 1)
             result = true;
         return result;
+    }
+
+    static Employee createEmployee() {
+        int employeeClass = generateEmployeeClass();
+        if (employeeClass == 1) {
+            return new Human();
+        } else if (employeeClass == 2) {
+            return new Robot();
+        } else if (employeeClass == 3) {
+            return new Alien();
+        }
+        return null;
+    }
+
+    private static int generateEmployeeClass() {
+        return (int) (Math.random() * 3 + 1);
     }
 }
