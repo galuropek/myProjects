a777996bc6f39b9bda72c25cbb2d068a3a16d962
Галушка Игорь <galuropek@gmail.com>
2018-12-03 17:17:31 +0300

    practice: test

--- src/practice/tasks/Util.java
+++ src/practice/tasks/Util.java
@@ -2,22 +2,30 @@ package practice.tasks;
 
 public class Util {
 
-    public static int[][] generateMatrix(int sizeRow, int sizeColomn){
+    public static int[][] generateMatrix(int sizeRow, int sizeColomn) {
         int[][] matrix = new int[sizeRow][sizeColomn];
         for (int i = 0; i < sizeRow; i++) {
             for (int j = 0; j < sizeColomn; j++) {
-                matrix[i][j] = (int)(Math.random()*10);
+                matrix[i][j] = (int) (Math.random() * 10);
             }
         }
         return matrix;
     }
 
-    public static void printMatrix(int[][] matrix){
+    public static String printMatrix(int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{{");
+        String bracket = "";
         for (int i = 0; i < matrix.length; i++) {
+            String separator = "";
+            sb.append(bracket);
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]);
+                sb.append(separator).append(matrix[i][j]);
+                separator = ", ";
+                bracket = "}, {";
             }
-            System.out.println();
         }
+        sb.append("}}");
+        return sb.toString();
     }
 }
--- src/practice/tasks/UtilTest.java
+++ src/practice/tasks/UtilTest.java
@@ -9,12 +9,18 @@ public class UtilTest {
     @Test
     public void generateMatrix() {
         int[][] matrix;
-        assertNotNull(matrix = Util.generateMatrix(3,3));
-        matrix = Util.generateMatrix(3,4);
+        assertNotNull(matrix = Util.generateMatrix(3, 3));
+        matrix = Util.generateMatrix(3, 4);
         assertTrue(matrix.length == 3 && matrix[0].length == 4);
     }
 
     @Test
     public void printMatrix() {
+        int[][] matrix = {{2, 3, 4}, {3, 4, 6}, {8, 0, 1}, {2, 7, 1}};
+        assertEquals("{{2, 3, 4}, {3, 4, 6}, {8, 0, 1}, {2, 7, 1}}"
+                , Util.printMatrix(matrix));
+        int[][] matrix2 = {{1, 2, 3}, {3, 2, 1}, {1, 2, 3}};
+        assertEquals("{{1, 2, 3}, {3, 2, 1}, {1, 2, 3}}"
+                , Util.printMatrix(matrix2));
     }
 }
