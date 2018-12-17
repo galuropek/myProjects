8eacc407e019557b0fc8454f9af7915db6570134
Галушка Игорь <galuropek@gmail.com>
2018-12-17 22:31:50 +0300

    practice: task 2.8

---
+++ src/practice/gerbageRecycler/CollectorTest.java
@@ -0,0 +1,13 @@
+package practice.gerbageRecycler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CollectorTest {
+
+    @Test
+    public void takeGarbage() {
+
+    }
+}
