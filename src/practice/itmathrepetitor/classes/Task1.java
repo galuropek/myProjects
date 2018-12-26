799715f9877af2a771d1161e01046c0509429a84
Галушка Игорь <galuropek@gmail.com>
2018-12-26 18:34:24 +0300

    practice: classes task1

--- src/practice/itmathrepetitor/classes/Task1.java
+++ src/practice/itmathrepetitor/classes/Task1.java
@@ -1,14 +1,31 @@
 package practice.itmathrepetitor.classes;
 
+import java.util.Arrays;
+
 public class Task1 {
 
     public static void main(String[] args) {
         Student[] students = initStudent();
+        Student[] sortedStudents = sortStudents(students);
+    }
+
+    private static Student[] sortStudents(Student[] students) {
+        Student[] sortedStudents = Arrays.copyOf(students, students.length);
+        for (int i = 0; i < sortedStudents.length; i++) {
+            Student student = sortedStudents[i];
+            int[] marks = student.getMarks();
+            double avg = 0.0;
+            for (int j = 0; j < marks.length; j++) {
+                avg = avg + marks[j];
+            }
+            student.setAvg(avg / marks.length);
+        }
+        return students;
     }
 
     private static Student[] initStudent() {
         Student[] students = new Student[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < students.length; i++) {
             Student student = new Student(
                     "Name" + i,
                     "Surname" + i,
