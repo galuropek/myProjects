6831c0f94aa5496ae749e4726ede2bfaa7903280
Галушка Игорь <galuropek@gmail.com>
2019-01-01 23:07:36 +0300

    task1

--- src/practice/itmathrepetitor/tasks/Task1.java
+++ src/practice/itmathrepetitor/tasks/Task1.java
@@ -3,13 +3,32 @@ package practice.itmathrepetitor.tasks;
 public class Task1 {
 
     public static void main(String[] args) {
-        System.out.println(getFactorial(5));
+        for (int i = 0; i < 10; i++) {
+            System.out.print("i: " + i + " ");
+            System.out.printf("%7d ", getFactorial(i));
+            System.out.printf("%7d \n", getFactorialRecursion(i));
+        }
     }
 
     private static int getFactorial(int number) {
         int result = 1;
         for (int i = 1; i <= number; i++) {
             result = result * i;
+        }
+        return result;
+    }
+
+    private static int getFactorialRecursion(int number) {
+        int defaultIndex = 1;
+        int defaultResult = 1;
+        return getFactorialRecursion(number, defaultIndex, defaultResult);
+    }
+
+    private static int getFactorialRecursion(int number, int index, int result) {
+        while (index <= number) {
+            result = result * index;
+            index++;
+            getFactorialRecursion(number, index, result);
         }
         return result;
     }
